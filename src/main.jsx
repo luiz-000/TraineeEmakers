1acbabef0a3710d7b26a07a9084c1b41950c1c4f
e <luizeduardoferreirasouza@gmail.com>
2024-12-12 14:56:56 -0300

    Alterações no css das pages Login - RecuperarConta - Registrar

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { createRoot } from 'react-dom/client'
 import Login from './pages/Login/Login.jsx';
 import Registrar from './pages/Registro/Registrar.jsx';
 import RecuperarConta from './pages/RecuperarConta/RecuperarConta.jsx';
+import Conta from './pages/TelaConta/Conta.jsx';
 
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
@@ -20,6 +21,10 @@ const router = createBrowserRouter ([
   {
     path: "/recuperarConta",
     element: <RecuperarConta/>
+  },
+  {
+    path: "/conta",
+    element: <Conta/>,
   },
 
 ]);
