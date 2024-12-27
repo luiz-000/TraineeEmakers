09722d9113b060d9c10966bbc1ae04863a53c7d8
e <luizeduardoferreirasouza@gmail.com>
2024-12-27 09:54:30 -0300

    Alterações no componente Carrossel

--- src/components/Carrossel/Carrossel.jsx
+++ src/components/Carrossel/Carrossel.jsx
@@ -1,48 +1,128 @@
 import React from 'react';
 
+import { IoIosArrowBack , IoIosArrowForward } from "react-icons/io";
+
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 import './Carrossel.css';
 
+
+/*function SampleNextArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block", background: "transparent"}}
+      onClick={onClick}
+    >
+      <button className='arrow'> <IoIosArrowForward className='icone'/> </button>
+    </div>
+  );
+}
+
+function SamplePrevArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block", background: "transparent"}}
+      onClick={onClick}
+    >
+      <button className='arrow'> <IoIosArrowBack className='icone'/> </button>
+    </div>
+  );
+}*/
+
 const Carrossel = () => {
+
+  const slides = [
+    {
+      imgEsquerda1: 'src/assets/imagens/imagemJogoDays.png',
+      imgEsquerda2: 'src/assets/imagens/imagemJogoHorizon.png',
+      imgDireita: 'src/assets/imagens/imagemJogoGod2.png',
+      precoEsq1: 'R$ 169,90',
+      descontoEsq1: '-15%',
+      precoEsq2: 'R$ 149,99',
+      descontoEsq2: '-25%',
+      precoDireita: 'R$ 119,95',
+      descontoDireita: '-40%',
+    },
+    {
+      imgEsquerda1: 'src/assets/imagens/imagemJogoMortal.jpg',
+      imgEsquerda2: 'src/assets/imagens/imagemJogoSkyrim.jpg',
+      imgDireita: 'src/assets/imagens/imagemJogoDeath2.jpg',
+      precoEsq1: 'R$ 80,99',
+      descontoEsq1: '-50%',
+      precoEsq2: 'R$ 110,90',
+      descontoEsq2: '-10%',
+      precoDireita: 'R$ 140,00',
+      descontoDireita: '-20%',
+    },
+    {
+      imgEsquerda1: 'src/assets/imagens/imagemJogoOri.avif',
+      imgEsquerda2: 'src/assets/imagens/imagemJogoRedDead.png',
+      imgDireita: 'src/assets/imagens/imagemJogoTLU.webp',
+      precoEsq1: 'R$ 115,00',
+      descontoEsq1: '-30%',
+      precoEsq2: 'R$ 55,99',
+      descontoEsq2: '-35%',
+      precoDireita: 'R$ 90,99',
+      descontoDireita: '-45%',
+    },
+  ];
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
   return (
     <div className='containerCarrossel'>
+      <Slider {...settings}>
 
-      <div className='imagensCarrossel'>
-
-        <div className='smallImages'>
+        {slides.map((slide, index) => (
           
-          <img src='/src/assets/imagens/imagem2Carrossel.png'></img>
-          <div className='item1'>
-            <p className='desconto'> -15% </p>
-            <p className='preço'> R$ 169,90 </p>
+          <div key={index}>
+            <div className='grid'>
+
+              <div className='direito'>
+                <img loading='lazy' src={slide.imgDireita}></img>
+
+                <div className='textoDireito'>
+                  <p className='desconto'> {slide.descontoDireita} </p>
+                  <p className='preco'> {slide.precoDireita} </p>
+                </div>
+              </div>
+
+              <div className='esquerdo'>
+                <img loading='lazy' src={slide.imgEsquerda1}></img>
+
+                <div className='textoEsquerdo'>
+                  <p className='desconto'> {slide.descontoEsq1} </p>
+                  <p className='preco'> {slide.precoEsq1} </p>
+                </div>
+              </div>
+            
+              <div className='esquerdo'>
+                <img loading='lazy' src={slide.imgEsquerda2}></img>
+
+                <div className='textoEsquerdo'>
+                  <p className='desconto'> {slide.descontoEsq2} </p>
+                  <p className='preco'> {slide.precoEsq2} </p>
+                </div>
+              </div>
+
+            </div>
           </div>
-          
-          <img src='/src/assets/imagens/imagem3Carrossel.png'></img>
-          <div className='item2'>
-            <p className='desconto'> -25% </p>
-            <p className='preço'> R$ 149,99 </p>
-          </div>
 
-        </div>
-
-        <div className='largeImages'>
-
-          <img src='/src/assets/imagens/imagem1Carrossel.png'></img>
-          <div className='item3'>
-            <p className='desconto'> -40% </p>
-            <p className='preço'> R$ 119,95 </p>
-          </div>
-          
-        </div>
-
-        <ul className='indicadores'>
-          <li className='indicador'></li>
-          <li className='indicador'></li>
-          <li className='indicador'></li>
-          <li className='indicador'></li>
-        </ul>
+        ))}
         
-      </div>
-
+      </Slider>
     </div>
   )
 }
