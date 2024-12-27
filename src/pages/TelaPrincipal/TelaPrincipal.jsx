e86092a66cc7e6de3428d158bd06ad1daef26dc5
e <luizeduardoferreirasouza@gmail.com>
2024-12-27 09:53:43 -0300

    Importando os componentes Carrossel Lançamentos e Footer para a page principal

--- src/pages/TelaPrincipal/TelaPrincipal.jsx
+++ src/pages/TelaPrincipal/TelaPrincipal.jsx
@@ -4,12 +4,18 @@ import './TelaPrincipal.css';
 
 import NavBar from '../../components/navBar/navBar';
 import Carrossel from '../../components/Carrossel/Carrossel';
+import CarrosselLancamentos from '../../components/CarrosselLancamentos/CarrosselLancamentos';
+import Footer from '../../components/Footer/Footer';
 
 const TelaPrincipal = () => {
   return (
     <div className='containerPage'>
         <NavBar />
-        <Carrossel />
+        <main>
+          <Carrossel />
+          <CarrosselLancamentos />
+        </main>
+        <Footer />
     </div>
   )
 }
