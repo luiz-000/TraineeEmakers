04ab3c009fd1441629f1fdf2dac763ee2f60d9f9
e <luizeduardoferreirasouza@gmail.com>
2024-12-30 01:51:51 -0300

    Inserindo novos componentes na page e ajustando o css da mesma

--- src/pages/TelaPrincipal/TelaPrincipal.jsx
+++ src/pages/TelaPrincipal/TelaPrincipal.jsx
@@ -5,15 +5,23 @@ import './TelaPrincipal.css';
 import NavBar from '../../components/navBar/navBar';
 import Carrossel from '../../components/Carrossel/Carrossel';
 import CarrosselLancamentos from '../../components/CarrosselLancamentos/CarrosselLancamentos';
+import CarrinhoCompra from '../../components/CarrinhoCompra/CarrinhoCompra';
 import Footer from '../../components/Footer/Footer';
 
 const TelaPrincipal = () => {
   return (
-    <div className='containerPage'>
+    <div className='containerPagePrincipal'>
         <NavBar />
         <main>
           <Carrossel />
           <CarrosselLancamentos />
+
+          <div className='posicaoCarrinhoCompra'>
+            <div className='carrinhoCompraTelaPrincipal'>
+              <CarrinhoCompra />
+            </div>
+          </div>
+
         </main>
         <Footer />
     </div>
