679a3cd384f12fe768830743ae4e2d324edc6f62
e <luizeduardoferreirasouza@gmail.com>
2024-12-30 01:45:05 -0300

    Alterando as rotas do arquivo main para o arquivo App.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,41 +1,13 @@
+import React from 'react';
+
+import App from './App.jsx';
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-import Login from './pages/Login/Login.jsx';
-import Registrar from './pages/Registro/Registrar.jsx';
-import RecuperarConta from './pages/RecuperarConta/RecuperarConta.jsx';
-import Conta from './pages/TelaConta/Conta.jsx';
-import TelaPrincipal from './pages/TelaPrincipal/TelaPrincipal.jsx';
-
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-
-
-const router = createBrowserRouter ([
-  {
-    path: "/",
-    element: <Login/>
-  },
-  {
-    path: "/registrar",
-    element: <Registrar/>
-  },
-  {
-    path: "/recuperarConta",
-    element: <RecuperarConta/>
-  },
-  {
-    path: "/conta",
-    element: <Conta/>
-  },
-  {
-    path: "/principal",
-    element: <TelaPrincipal/>
-  },
-
-]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router}/>
+    <App />
   </StrictMode>,
 )
