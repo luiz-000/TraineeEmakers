5a45e62af1f3d0e8c4e94e6beb511b1db408ea8e
Luiz Eduardo <luizeduardoferreirasouza@gmail.com>
2025-01-03 20:22:58 -0300

    Criando a page Tela jogo

--- src/pages/TelaJogo/TelaJogo.jsx
+++ src/pages/TelaJogo/TelaJogo.jsx
@@ -4,12 +4,22 @@ import './TelaJogo.css';
 
 import NavBar from '../../components/NavBar/NavBar';
 import InfoJogo from '../../components/InfoJogo/InfoJogo';
+import CarrinhoCompra from '../../components/CarrinhoCompra/CarrinhoCompra';
+import Footer from '../../components/Footer/Footer';
 
 const TelaJogo = () => {
   return (
     <div className='containerTelaJogo'>
       <NavBar />
       <InfoJogo />
+      
+      <div className='posicaoCarrinhoCompraTelaJogo'>
+        <div className='CarrinhoCompraTelaJogo'>
+          <CarrinhoCompra />
+        </div>
+      </div>
+
+      <Footer />
     </div>
   )
 }
