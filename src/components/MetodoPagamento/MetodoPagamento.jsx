4d4e9e41ceb07e21ee625ef861eb401b95b1aaa4
Luiz Eduardo <luizeduardoferreirasouza@gmail.com>
2025-01-03 20:16:17 -0300

    Criando o componente metodo de pagamento da page Tela Carrinho

---
+++ src/components/MetodoPagamento/MetodoPagamento.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import './MetodoPagamento.css';
+
+import { PiShoppingCartSimpleFill } from "react-icons/pi";
+
+const MetodoPagamento = () => {
+  return (
+    <div className='containerMetodoPagamento'>
+        
+        <div className='cardMetodoPagamento'>
+            <h2> Método de Pagamento </h2>
+            
+            <div className='itensMetodoPagamento'>
+
+                <div className='itemMetodoPagamento'>
+                    <img loading='lazy' src='src/assets/imagens/imagemPix.png'></img>
+                    <p> Pix </p>
+                    <input name='formaPagamento' value="pix" type='radio' />
+                </div>
+
+                <div className='itemMetodoPagamento'>
+                    <hr></hr>
+                </div>
+
+                <div className='itemMetodoPagamento'>
+                    <input name='termoCondicao' value="aceito" type='radio' />
+                    <label className='termosMetodoPagamento'> Eu aceito os termos e condições </label>
+                </div>
+
+                <div className='itemMetodoPagamento'>
+                    <button> Finalizar Compra <PiShoppingCartSimpleFill className='iconeFinalizarMetodoPagamento'></PiShoppingCartSimpleFill> </button>
+                </div>
+                
+            </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default MetodoPagamento
