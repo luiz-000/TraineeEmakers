7828c3ccc8f29ea6f2514f09f01efb744107a188
Luiz Eduardo <luizeduardoferreirasouza@gmail.com>
2025-01-03 20:25:43 -0300

    Criando o componente Info Jogo

--- src/components/InfoJogo/InfoJogo.jsx
+++ src/components/InfoJogo/InfoJogo.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { PiShoppingCartSimpleFill } from "react-icons/pi";
+
 import './InfoJogo.css';
 
 const InfoJogo = () => {
@@ -9,8 +11,16 @@ const InfoJogo = () => {
         <div className='layoutInfoJogo'>
             
             <div className='imagemInfoJogo'>
+
+                <p className='porcentagemInfoJogo'> 98% </p>
                 <img loading='lazy' src='src\assets\imagens\imagemJogoGod2.png'></img>
+                
+                <div className='valoresInfoJogo'>
+                    <p className='descontoInfoJogo'> -40% </p>
+                    <p className='precoInfoJogo'> R$ 119,95 </p>
+                </div>
             </div>
+
             <div className='textoInfoJogo'>
                 <h2> God of War </h2>
                 <p> Com a vingança contra os deuses do Olimpo em um passado distante, Kratos
@@ -20,8 +30,12 @@ const InfoJogo = () => {
                 </p>
                 
                 <div className='buttonsInfoJogo'>
-                    <button> Adicionar ao Carrinho </button>
-                    <button className='buttonReembolsoInfoJogo'> Solicitar Reembolso </button>
+                    <button>
+                        Adicionar ao Carrinho <PiShoppingCartSimpleFill className='iconeCartTelaJogo'/>
+                    </button>
+                    <button className='buttonReembolsoInfoJogo'>
+                        Solicitar Reembolso <PiShoppingCartSimpleFill className='iconeCartTelaJogo' />
+                    </button>
                 </div>
                 
             </div>
