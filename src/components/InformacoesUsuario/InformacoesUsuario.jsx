c94cf6bc515bdb6767868a1d9be2b37ea6af8509
Luiz Eduardo <luizeduardoferreirasouza@gmail.com>
2025-01-03 20:26:33 -0300

    Atualizando o código para receber os dados enviados da page Login

--- src/components/InformacoesUsuario/InformacoesUsuario.jsx
+++ src/components/InformacoesUsuario/InformacoesUsuario.jsx
@@ -1,23 +1,33 @@
 import React from 'react'
 
+import { Link , useLocation } from 'react-router-dom';
 import { MdModeEdit } from "react-icons/md";
 import { RiDeleteBin5Line } from "react-icons/ri";
 
 import './InformacoesUsuario.css'
 
 const InformacoesUsuario = () => {
+
+  const location = useLocation();
+  const userEmail = location.state?.userEmail;
+
   return (
     <div className='containerEsquerdo'>
 
       <div className='dadosUsuario'>
-        <h1> Olá, --Nome do Usuário-- </h1>
-        <p> Seu email é nomeusuario@email.com </p>
+        <h1> Olá, {userEmail} </h1>
+        <p> Seu email é {userEmail} </p>
         <p> Seu CPF é 123.456.789-00 </p>
 
         <button> Alterar Dados <MdModeEdit className='iconeEditar' /> </button>
-        <button> Excluir Conta <RiDeleteBin5Line className='iconeDelete' /> </button>
         
-        <button className='buttonAdmin'> Tela do admin </button>
+        <button>
+          <Link to='/'> Excluir Conta <RiDeleteBin5Line className='iconeDelete' /> </Link>
+        </button>
+        
+        <button className='buttonAdmin'>
+          <Link to='/adm'> Tela do admin </Link>
+        </button>
       </div>
 
     </div>
