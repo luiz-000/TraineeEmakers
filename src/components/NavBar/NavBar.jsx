77a1c4871920bdced635e3062ee8534490d29cdf
Luiz Eduardo <luizeduardoferreirasouza@gmail.com>
2025-01-04 20:46:37 -0300

    Inserindo responsividade na NavBar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,24 +1,33 @@
-import React from 'react';
+import { React , useState } from 'react';
 
-import { LuSearch } from "react-icons/lu";
+import { LuSearch , LuMenu, LuX } from 'react-icons/lu';
 import { Link } from 'react-router-dom';
 
 import './NavBar.css';
 
 
 const NavBar = () => {
+
+    const [openMenu, setOpenMenu] = useState(false);
+
+    const toggleMenu = () => {
+        setOpenMenu(!openMenu);
+    };
+
     return (
         <div>
             <nav className='navBar'>
 
                 <div className="navbarImg">
-                    <img src="/src/assets/imagens/imagem1.png"/>
+                    <Link to='/principal'>
+                        <img src="/src/assets/imagens/imagem1.png" alt='imagem da logo da empresa'/>
+                    </Link>
                 </div>
 
-                <ul className="navbarLinks">
+                <ul className={`navbarLinks ${openMenu ? 'open' : ''}`}>
                     <li> <Link to='/principal'> Lançamentos </Link> </li>
                     <li> <Link to='/populares'> Populares </Link> </li>
-                    <li><a href="#"> Gêneros </a></li>
+                    <li> <Link to=''> Gêneros </Link> </li>
                     <li> <Link to='/principal'> Promoções </Link> </li>
                     <li> <Link to='/conta'> Conta </Link> </li>
                 </ul>
@@ -27,6 +36,10 @@ const NavBar = () => {
                     <input type="text"/>
                     <button> <LuSearch className='iconeBusca'/> </button>
                 </div>
+
+                <button className='buttonMenuNavBar' onClick={toggleMenu}>
+                    {openMenu ? <LuX className='iconeMenuNavBar' /> : <LuMenu className='iconeMenuNavBar'/>}
+                </button>
 
             </nav>
         </div>
